3c8f59fbf03969a0e23a7ea4c230ff93786a929b
Kimkax <166659494+Kimkax@users.noreply.github.com>
2024-04-11 15:32:26 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,16 +2,16 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['254787797124']
+global.ownername = "huntervegaz"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
 
-global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
+global.botname = 'Shiku' //name of the bot
 
 //sticker details
-global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
+global.stickername = 'huntervegas'
 global.packname = 'Sticker By'
 global.author = 'Maria Bot'
 //console view/theme
